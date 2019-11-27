18940fdb90069a23ff29af2c42b35ba9c8d4e462
Gauri <gauri.kangai@outlook.com>
2019-11-27 20:12:40 +0000

    Practising exercises again to recap learning of Loops

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,32 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstname, lastname) {
+    return firstname + " " + lastname;
+}
+
+function makeHalfPrice(price) {
+    return price * 0.5;
+}
+
+function countBooks(books) {
+    return books.length;
+}
+
+function isInStock(bookshelf) {
+    for (let i = 0; i < bookshelf.length; i++) {
+    if (bookshelf.quantity >= 1) {
+        return true;
+    }
+}
+return false;
+}
+
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -4,8 +4,46 @@ describe("utils.addVAT", () => {
   test("returns 36 when passed 30", () => {
     expect(utils.addVAT(30)).toBe(36);
   });
-  
+
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 240 when passed 200", () => {
+    expect(utils.addVAT(200)).toBe(240);
+  });
+
 });
+
+describe("utils.getFullName", () => {
+  test("returns Gauri Kangai when passed Gauri and Kangai", () => {
+    expect(utils.getFullName("Gauri", "Kangai")).toBe("Gauri Kangai");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 50 when passed 100", () => {
+    expect(utils.makeHalfPrice(100)).toBe(50);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("returns 3 when passed Harry Potter, Shantaram, The Way Of The Turtle", () => {
+    expect(utils.countBooks(["Harry Potter", "Shantaram", "The Way Of The Turtle"])).toBe(3);
+  });
+});
+
+describe("utils.isInStock", () => {
+  test("returns true if passed a quantity equal or greater to 1", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 9
+    };
+    expect(utils.isInStock("The Stone Diaries")).toBe(true);
+  });
+});
+
+
+
